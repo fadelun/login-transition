b15ac720d7a388e26abe8ceb809aac69eef255fd
Muhammad Fadll <fadeljourney@gmail.com>
2022-11-20 17:30:53 +0700

    add: menambahkan transisi

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ const buttonEnter = document.querySelectorAll('.btn-enter')
 const formPage = document.querySelector(".form-page")
 const formWrapperOne = document.querySelector(".form-wrapper-one")
 const formWrapperTwo = document.querySelector(".form-wrapper-two")
+const messageOne = document.querySelector(".message-one")
 
 function handleButtonEnter() {
     if (formPage.classList.contains("side-move")) {
         formPage.classList.remove("side-move");
-        formWrapperOne.classList.remove("form-wrapper-move")
+        formWrapperOne.classList.remove("form-wrapper-hidden")
+        formWrapperTwo.classList.add('form-wrapper-hidden')
+        messageOne.classList.remove("message-hidden")
     } else {
         formPage.classList.add('side-move')
-        formWrapperOne.classList.add('form-wrapper-move')
+        formWrapperOne.classList.add('form-wrapper-hidden')
+        formWrapperTwo.classList.remove("form-wrapper-hidden")
+        messageOne.classList.add("message-hidden")
     }
     // displayTransition()
 }
